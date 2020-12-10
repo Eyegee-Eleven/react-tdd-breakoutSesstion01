f013631cf8e6f94f49da298285f86873a58572a9
Cecil Montemayor <cecilmontemayoriv@gmail.com>
2020-12-10 12:59:29 -0500

    breakout room closed stopping point left off at passing render test

---
+++ src/PersonList.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import App from './App.js';
+
+class PersonList extends Component{
+
+  render(){
+  return(
+    <>
+    </>
+  )
+  }
+}
+
+
+
+
+
+
+
+
+
+export default PersonList
